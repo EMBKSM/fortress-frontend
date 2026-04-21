5c5806c8e6304f823f037406b341ecfdf8ff84b9
김성민 <117885263+EMBKSM@users.noreply.github.com>
2026-04-21 22:38:26 +0900

    Change server URL to production endpoint

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -2,7 +2,7 @@
 import { io } from 'socket.io-client';
 
 // 로컬 테스트용 주소. 나중에 Render에 배포하면 그 주소로 바꿀 겁니다!
-const SERVER_URL = 'http://localhost:3001';
+const SERVER_URL = 'https://fortress-backend-kh3x.onrender.com';
 
 export const socket = io(SERVER_URL, {
     autoConnect: false, // 앱이 켜지자마자 연결하지 않고, '시작' 버튼을 눌렀을 때만 연결
